28fa1ff8fa26a27409da95c8da0a37c86fc672e6
jin camou <jincamou@gmail.com>
2017-05-02 21:04:07 -0700

    streamline erb files, debug word.erb

--- spec/dictionary_integration_spec.rb
+++ spec/dictionary_integration_spec.rb
@@ -4,15 +4,47 @@ require "./app"
 Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
 
-describe("the dictionary path", {:type => :feature}) do
-
-  it("allows user to enter and store words and definitions") do
+describe("adding a new word", {:type => :feature}) do
+  it("allows user to click a word to see definitions for it") do
     visit("/")
-    click_on('Add a new word')
+    click_link('Add a new word')
     fill_in("new_word", :with => "arch")
     click_button('Add Word')
     expect(page).to have_content("arch")
   end
+end
+
+describe('viewing a list of all of the words', {:type => :feature}) do
+  it('allows a user to see all of the words that have been created') do
+    word = Word.new({:new_word => "arch", :id => 1, :term => "definition"})
+    word.save
+    visit('/')
+    click_link("See my list of words")
+    expect(page).to have_content(word.new_word)
+  end
+end
+
+describe('seeing definitions for a single word', {:type => :feature}) do
+  it('allows a user to click a word to see the definitions for it') do
+    test_word = Word.new({:new_word => "cat", :id => 1, :term => "definition"})
+    test_word.save()
+    test_definition = Definition.new({:define_word => "a feline mammal", :id => 1})
+    test_definition.save()
+    visit('/words')
+  end
+end
+
+describe('adding definitions to a word', {:type => :feature}) do
+  it('allows a user to add a definition to a word') do
+    test_word = Word.new({:new_word => "cat", :id => 1 })
+    test_word.save()
+    visit("/definitions")
+    fill_in("define_word", {:with => "a feline mammal"})
+    click_button("Add Definition")
+    visit("/word")
+    expect(page).to have_content("Here are your definitions for this word:")
+  end
+end
+
 
   # fill_in("definition", :with => "a curved symmetrical structure spanning an opening and typically supporting the weight of a bridge, roof, or wall above it.")
-end
